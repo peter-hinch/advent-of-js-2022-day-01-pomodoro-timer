9a12f04a370e2fc2dbd95583b9f32d6c3e9a7463
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2023-10-10 11:02:59 +1100

    Store time in seconds only and convert on display.

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 const App = () => {
-  const [timeRemaining, setTimeRemaining] = useState({
-    minutes: 15,
-    seconds: 0
-  });
+  const [timeRemaining, setTimeRemaining] = useState<number>(900);
   const [timerRunning, setTimerRunning] = useState<boolean>(false);
 
-  const padTimeValue = (val: number) => val.toString().padStart(2, '0');
+  const convertSeconds = (value: number) => {
+    const minutes = (value / 60).toString().padStart(2, '0');
+    const seconds = (value % 60).toString().padStart(2, '0');
+
+    return { minutes, seconds };
+  };
 
   useEffect(() => {
     console.log('timerRunning:', timerRunning);
@@ -22,9 +24,13 @@ const App = () => {
         <div className="timer__face"></div>
         <div className="timer__content">
           <span className="countdown__time-remaining">
-            <span id="time__mins">{padTimeValue(timeRemaining?.minutes)}</span>
+            <span id="time__mins">
+              {convertSeconds(timeRemaining)?.minutes}
+            </span>
             <span>:</span>
-            <span id="time__secs">{padTimeValue(timeRemaining?.seconds)}</span>
+            <span id="time__secs">
+              {convertSeconds(timeRemaining)?.seconds}
+            </span>
           </span>
           <button
             className="countdown__button-start-stop"
